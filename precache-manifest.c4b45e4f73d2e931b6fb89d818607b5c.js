a7b388e779511d670c49e21caf75819246511266
Bimochan Shrestha <bmochan@gmail.com>
2019-12-20 00:24:20 +0545

    Updates

---
+++ precache-manifest.c4b45e4f73d2e931b6fb89d818607b5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e9c6bbb178637d3d9d0f41907e3f5c",
+    "url": "/boggle/index.html"
+  },
+  {
+    "revision": "314340c4835b0ada22e6",
+    "url": "/boggle/static/css/2.bea6f0b1.chunk.css"
+  },
+  {
+    "revision": "2307c4d880999f2bebba",
+    "url": "/boggle/static/css/main.abb548e0.chunk.css"
+  },
+  {
+    "revision": "314340c4835b0ada22e6",
+    "url": "/boggle/static/js/2.722a989a.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/boggle/static/js/2.722a989a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2307c4d880999f2bebba",
+    "url": "/boggle/static/js/main.7b0ebd3b.chunk.js"
+  },
+  {
+    "revision": "0551b66d21434d2fd714",
+    "url": "/boggle/static/js/runtime-main.6db15363.js"
+  }
+]);
